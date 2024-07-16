baccf91f870d9a80ab1f42f432b3ba57ede12e7a
harris-ahmad <24100315@lums.edu.pk>
2024-07-17 00:45:41 +0500

    db connection added

--- AuthService/go.mod
+++ AuthService/go.mod
@@ -3,6 +3,8 @@ module BackendEngineeringTest/AuthService
 go 1.22.5
 
 require (
+	github.com/joho/godotenv v1.5.1
+	github.com/streadway/amqp v1.1.0
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.34.2
 )
--- AuthService/main.go
+++ AuthService/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	pb "BackendEngineeringTest/AuthService/proto"
 	"database/sql"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
@@ -19,13 +22,35 @@ var (
 	channel *amqp.Channel
 )
 
+func init() {
+	var err error
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
+
+	var (
+		dbUser string = os.Getenv("DB_USER")
+		dbPass string = os.Getenv("DB_PASS")
+		dbName string = os.Getenv("DB_NAME")
+		dbHost string = os.Getenv("DB_HOST")
+		dbPort string = os.Getenv("DB_PORT")
+	)
+
+	connString := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable", dbUser, dbPass, dbName, dbHost, dbPort)
+	db, err = sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+}
+
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	rpcServer := grpc.NewServer()
-	// Register the service
 	pb.RegisterAuthServiceServer(rpcServer, &AuthService{})
 	if err := rpcServer.Serve(listener); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
